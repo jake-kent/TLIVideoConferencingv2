120cb40ae488ec8004d13dbf4a66dfb2c181aaac
Jacob Kent <kentjakel@wustl.edu>
2017-07-13 17:18:58 +0800

    step up bandwidth

--- kurento-group-call/src/main/java/org/kurento/tutorial/groupcall/UserSession.java
+++ kurento-group-call/src/main/java/org/kurento/tutorial/groupcall/UserSession.java
@@ -78,8 +78,8 @@ public class UserSession implements Closeable {
     this.roomName = roomName;
     this.isRecording = false;
     this.outgoingMedia = new WebRtcEndpoint.Builder(pipeline).build();
-    this.outgoingMedia.setMinVideoRecvBandwidth(500);
-    this.outgoingMedia.setMinVideoSendBandwidth(500);
+    this.outgoingMedia.setMinVideoRecvBandwidth(1000);
+    this.outgoingMedia.setMinVideoSendBandwidth(1000);
     this.outgoingMedia.setMaxVideoRecvBandwidth(0);
     this.outgoingMedia.setMaxVideoSendBandwidth(0);
     this.outgoingMedia.addIceCandidateFoundListener(new EventListener<IceCandidateFoundEvent>() {
