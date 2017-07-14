ac3b5ff24426a400778f41d584610f9a4c415b74
Jacob Kent <kentjakel@wustl.edu>
2017-07-14 09:54:21 +0800

    update bitrates to all 2000

--- kurento-group-call/src/main/java/org/kurento/tutorial/groupcall/UserSession.java
+++ kurento-group-call/src/main/java/org/kurento/tutorial/groupcall/UserSession.java
@@ -78,10 +78,12 @@ public class UserSession implements Closeable {
     this.roomName = roomName;
     this.isRecording = false;
     this.outgoingMedia = new WebRtcEndpoint.Builder(pipeline).build();
+
     this.outgoingMedia.setMinVideoRecvBandwidth(2000);
     this.outgoingMedia.setMinVideoSendBandwidth(2000);
-    this.outgoingMedia.setMaxVideoRecvBandwidth(0);
-    this.outgoingMedia.setMaxVideoSendBandwidth(0);
+    this.outgoingMedia.setMaxVideoRecvBandwidth(2000);
+    this.outgoingMedia.setMaxVideoSendBandwidth(2000);
+
     this.outgoingMedia.addIceCandidateFoundListener(new EventListener<IceCandidateFoundEvent>() {
 
       @Override
