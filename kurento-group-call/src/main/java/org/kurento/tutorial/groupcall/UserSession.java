cefb999c1711faf6ad33cf74f16c16b283e64e5f
Jacob Kent <kentjakel@wustl.edu>
2017-07-20 09:50:05 +0800

    run conversion from webm to hls

--- kurento-group-call/src/main/java/org/kurento/tutorial/groupcall/UserSession.java
+++ kurento-group-call/src/main/java/org/kurento/tutorial/groupcall/UserSession.java
@@ -305,20 +305,8 @@ public class UserSession implements Closeable {
     if (pendingConversion == true) {
       log.info("Run Conversion");
       try {
-        String result = null;
-        Runtime r = Runtime.getRuntime();                    
-
-        Process p = r.exec("ffmpeg -i " + RECORDING_PATH + preConvertedName + RECORDING_EXT + " -strict -2 -q:vscale 0 " + RECORDING_PATH + preConvertedName + ".mp4");
-
-        BufferedReader in =
-            new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            System.out.println(inputLine);
-            result += inputLine;
-        }
-        in.close();
-        log.info("FFMPEG output: {}", result);
+        Runtime.getRuntime().exec("ffmpeg -i " + RECORDING_PATH + preConvertedName + RECORDING_EXT + " -profile:v baseline -level 3.0 -s 1280x960 -start_number 0 -hls_time 10 -hls_list_size 0 -strict -2 -f hls " + RECORDING_PATH + preConvertedName + ".m3u8");
+        //ffmpeg -i 2017-07-19_17-09-23-477-Teacher1-Room1.webm -profile:v baseline -level 3.0 -s 1280x960 -start_number 0 -hls_time 10 -hls_list_size 0 -strict -2 -f hls testout1.m3u8
       }
       catch (IOException e) {
         log.info(e.getMessage());
